d894414044b112a4a20c6cce4bd98659e6ebf3bc
kelar crisp <kelcrisp007@icloud.com>
2019-07-06 15:42:44 -0400

    ehh

--- src/main.js
+++ src/main.js
@@ -5,26 +5,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Constructor-Name } from './backend-code';
 
 window.addEventListener('load', ()=> {
-    let lat;
+  let lat;
   let long;
+  let temperatureDescription = document.querySelector(".temperature-description");
 
+  let temperatureDegree = document.querySelector(".temperature-degree");
 
+  let locationTimezone = document.querySelector(".location-timezone");
+
+  let temperatureSection = document.querySelector(".temperature-section");
+  
   if (navigator.geolocation){
   navigator.geolocation.getCurrentPosition(position =>{
     long = position.coords.longitude;
-    lat = position.coords.lttitude;
+    lat = position.coords.latitude;
 
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     const api = `${proxy}https://api.darksky.net/forecast/1ddc868dd4e1192dd84f96232c2cc0e0/${lat},${long}`;
 
-    fetch(api).then(response =>{
+    fetch(api)
+    .then(response => {
       return response.json();
     })
     .then(data =>{
-      console.log(data);
+      const { temperature,summary } = data.currently;
+      //set dom elements from the API
+      temperatureDegree.textContent = temperature;
+      temperatureDescription.textContent = summary;
+      locationTimezone.textContent = data.timezone;
     });
   });
   }
+  temperatureSection
 });
 
 
